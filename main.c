0c20328a9032ae27c5b9cfcc5d48d128c23ba46e
Andrii <andpris527@gmail.com>
2022-10-12 20:05:20 +0300

    Try to commit first practice

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <conio.h>
+
+int roman(int, int, char);
+
+int main() {
+    int a;
+
+    //get user's input
+    printf("Enter a number: \n");
+    scanf("%d", &a);
+
+    //convert into roman
+    a = roman(a, 1000, 'M');
+    a = roman(a, 500, 'D');
+    a = roman(a, 100, 'C');
+    a = roman(a, 50, 'L');
+    a = roman(a, 10, 'X');
+    a = roman(a, 5, 'V');
+    a = roman(a, 1, 'I');
+
+    getch();
+    return 0;
+}
+
+roman(int i, int j, char c) {
+    while(i >= j) {
+        putchar(c);
+        i -= j;
+    }
+
+    return i;
+}
