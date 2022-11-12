1d930d56dc5f56f1e86e86ca0151d6a20bcaaee9
Andrii <andpris527@gmail.com>
2022-11-12 22:28:34 +0200

    mark main branch

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int roman(int, int, char);
 
 int main() {
     int a;
-
+    printf("Main branch\n");
     //get user's input
     printf("Enter a number: \n");
     if(!scanf("%d", &a) || a <= 0) {
